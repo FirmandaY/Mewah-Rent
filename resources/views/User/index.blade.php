0d741af80a8a09eb8d15c543d8b64c1b69c64989
FirmandaY <firmandayahya01@mail.ugm.ac.id>
2021-12-09 19:07:26 +0700

    commit on 'master'

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -158,9 +158,12 @@
                         </div>
                        
                         <div class = "content">
-                           <a href="{{ route('user.produklain', $kategori->nama) }}" class="btn btn-primary">
-                              <h4>Lihat Produk</h4>
-                           </a>
+                           <form action="{{ route('user.produklain', $kategori->nama) }}" method="post">
+                              @csrf
+                              <button class="btn btn-primary">
+                                 <h4>Lihat Produk</h4>
+                              </button>
+                           </form>
                            <h3>{{ $kategori->nama }}</h3>
                            <p>{!! $kategori->keterangan !!}</p>
                         </div>
